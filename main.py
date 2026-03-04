c07e937f743a43c28cf7aba6acbec402ae73db51
NayanPython <choudharyji527@gmail.com>
2026-03-04 12:59:54 +0530

    Constructed API documentation page and API page itself

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from flask import Flask , render_template                                       #web framework like streamlit or django
 
-app = Flask("Website")
+app = Flask(__name__)
 
-@app.route("/home")
+
+@app.route("/")
 def home():
-    return render_template("webpage1.html")
+    return render_template("home.html")
 
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")                                     #special pattern for url symbol <> to add/change values in url
+def about(station, date):
+    temperature = 23
+    return {"station": station,                                          #gerated api data /23 is hard coded ,but we can change station and date from url
+            "date": date,
+            "temperature": temperature}
 
-app.run(debug=True)
+if __name__== "__main__":                                                  #so that we can run main.py here only
+    app.run(debug=True)
