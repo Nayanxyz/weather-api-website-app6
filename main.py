20f42929aab9b6c715842f98d5810a7fe79fe256
NayanPython <choudharyji527@gmail.com>
2026-03-04 19:30:12 +0530

    API works with data , used Jupyterlab to read Dataframes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template                                       #web framework like streamlit or django
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,13 +9,16 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/api/v1/<station>/<date>")                                     #special pattern for url symbol <> to add/change values in url
+@app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
-    return {"station": station,                                          #gerated api data /23 is hard coded ,but we can change station and date from url
-            "date": date,
+    """ copied path of the file , then str(station) is string and used .zfill( Pad a numeric string with zeros on the left,
+    to fill a field of the given width.) got 4 zeroes digit before station no.
+) """
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"         #used jupyterlab to get data systematically
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE']=="1860-01-05"]['   TG'].squeeze()/10
+    return {"station": station,
             "temperature": temperature}
 
-if __name__== "__main__":                                                  #so that we can run main.py here only.
-    app.run(debug=True)                                                   #we can run apps on  different port so that apps can run simultaneously, perfectly
-                                                                          #app.run(debug=True , port=5001)
+if __name__== "__main__":                                                  #we now have our working api
+    app.run(debug=True)
