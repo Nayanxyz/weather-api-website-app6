fde85e0ec407af3d2f07dc918e7760d71804b69b
NayanPython <choudharyji527@gmail.com>
2026-03-05 11:13:55 +0530

    Added endpoints for one station and yearly data in URL(REST API)

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def about(station, date):
     """ copied path of the file , then str(station) is string and used .zfill( Pad a numeric string with zeros on the left,
     to fill a field of the given width.) got 4 zeroes digit before station no.
 ) """
-    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"         #used jupyterlab to get data systematically
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"                      #used jupyterlab to get data systematically
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE']=="1860-01-05"]['   TG'].squeeze()/10
     return {"station": station,
@@ -27,8 +27,8 @@ def about(station, date):
 def one_station(station):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])                    #dates here and above are all in integer format
-    result = df.to_dict(orient="records")                                    #to_dict method used for converting data into dictionary
-    return result                                                            # and change data format to records
+    result = df.to_dict(orient="records")                                                #to_dict method used for converting data into dictionary
+    return result                                                                        # and change data format to records
 
 
 
@@ -41,8 +41,8 @@ def yearly(station, year):
     return result                                                                       #df['DATE'].str here .str is a bridge , it tells pandas
                                                                                         # "Look at every single item in this column and
                                                                                         # treat it as a string so I can use string methods on it."
-                                                                                        #str.startswith method used to ensure the year date starts with year and as string
+                                                                                        #str.startswith method used to ensure the year date starts with year
+                                                                                        # and as string ,str(year) is used
 
-
-if __name__== "__main__":                                                  #we now have our working Rest api
+if __name__== "__main__":                                                               #we now have our working Rest api
     app.run(debug=True)
