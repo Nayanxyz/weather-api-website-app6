2c1e0c9e49b752a201079ce27463e3121431d299
NayanPython <choudharyji527@gmail.com>
2026-03-05 10:54:12 +0530

    Added endpoints for one station and yearly data in URL(REST API)

--- main.py
+++ main.py
@@ -23,5 +23,24 @@ def about(station, date):
     return {"station": station,
             "temperature": temperature}
 
-if __name__== "__main__":                                                  #we now have our working api
+@app.route("/api/v1/<station>")
+def one_station(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])                    #dates here and above are all in integer format
+    result = df.to_dict(orient="records")                                    #to_dict method used for converting data into dictionary
+    return result                                                            # and change data format to records
+
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)                                            #changed dates to string
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
+
+if __name__== "__main__":                                                  #we now have our working Rest api
     app.run(debug=True)
